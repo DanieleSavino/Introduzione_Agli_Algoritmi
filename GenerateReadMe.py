690aec9a52ca3e0e1eb8c0d751b339d6d49be39d
DanieleSavino <savino.2140356@studenti.uniroma1.it>
2024-05-19 23:35:48 +0200

    Auto ReadMe

---
+++ GenerateReadMe.py
@@ -0,0 +1,43 @@
+import os
+
+DEFAULT = '''# Introduzione Agli Algoritmi
+Collezione di esercizi su algoritmi e strutture dati tratti da vecchi scritti del corso **Introduzione Agli Algoritmi** della Sapienza.\n
+'''
+
+def listFiles(path):
+    A = []
+    for file in os.listdir(path):
+        if file.startswith('__') or file.startswith('.'): continue
+        newPath = os.path.join(path, file)
+
+        if os.path.isfile(newPath):
+            A.append(file)
+        else:
+            A.extend(listFiles(newPath))
+
+    return A
+
+def mapFiles(path):
+    d = {s.replace('_', ' ') : listFiles(s) for s in os.listdir(path) if os.path.isdir(os.path.join(path, s)) and not s.startswith('.')}
+    return d
+
+def generateText(fileMap):
+    A = []
+    for k, v in fileMap.items():
+        A.append(f'## {k}\n')
+        for s in v:
+            A.append(f'- {s}\n')
+        A.append('\n')
+
+    return ''.join(A)
+
+def writeReadMe(text, path):
+    with open(path, 'w') as f:
+        f.write(DEFAULT + text)
+
+def main(path, readmePath):
+    fileMap = mapFiles(path)
+    text = generateText(fileMap)
+    writeReadMe(text, readmePath)
+
+main(os.getcwd(), 'README.md')
---
+++ README.md
@@ -0,0 +1,18 @@
+# Introduzione Agli Algoritmi
+Collezione di esercizi su algoritmi e strutture dati tratti da vecchi scritti del corso **Introduzione Agli Algoritmi** della Sapienza.
+
+## strutture dati
+- AlberoBinario.py
+- ListaLinkata.py
+
+## esercizi array
+- TrovaDuplicato.py
+- MaxSequenzaElementiUguali .py
+
+## esercizi albero
+- ContaNodi.py
+- MassimoCammino.py
+
+## esercizi liste linkate
+- MinimoMassimo.py
+
